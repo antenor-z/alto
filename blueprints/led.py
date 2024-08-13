9365197db0b1246b578dd1026a68dde4b9d7b5a6
antenor-z <antenor-z@pm.me>
2024-08-13 13:21:10 -0300

    use saved color on startup

--- blueprints/led.py
+++ blueprints/led.py
@@ -18,12 +18,7 @@ def write_color_to_file(color):
     with open("currentColor", "w") as fp:
         fp.write(color)
 
-@led.get("/currentColor")
-def get_current_color():
-    return get_current_color_from_file()
-
-@led.get("/writeColor/<string:color>")
-def write_color(color: str, save=True):
+def _write_color(color: str, save=True):
     if color.startswith("#"):
         color = color[1:]
     if len(color) != 6 or not all(c in "0123456789abcdefABCDEF" for c in color):
@@ -35,6 +30,14 @@ def write_color(color: str, save=True):
         write_color_to_file(color)
 
     return "ok"
+
+@led.get("/currentColor")
+def get_current_color():
+    return get_current_color_from_file()
+
+@led.get("/writeColor/<string:color>")
+def write_color(color: str):
+    return _write_color(color)
 
 def linear(start, end, steps, i):
     diff = end - start
@@ -52,7 +55,9 @@ def fade_color(start_color, end_color):
         time.sleep(1 / 30)
 
 def clean_exit():
-    write_color("000000", save=False)
+    _write_color("000000", save=False)
+
+write_color(get_current_color_from_file())
 
 atexit.register(clean_exit)
 
